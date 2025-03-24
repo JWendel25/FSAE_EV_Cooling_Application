1f7fd3de3e0e0a494da5f5a7a99c3a706d3989bf
Joshua Wendel <joshua.w.wendel@okstate.edu>
2025-03-24 14:33:25 -0500

    Spring Break Work
    Created the Startup UI

--- main_Landing.py
+++ main_Landing.py
@@ -1,10 +1,10 @@
 # Main Pseudo code
 
-# Connect to UI
 
-# Open Home Page
+# Open Home Page | Dialog
     # Probably just an 'Start Simulation' button
     # Changes window to 1st Initialization page
     # This may require seperate file???
-
-# 1st Initialization Page 
+    # I DON'T THINK I WANT THIS, CHANGE SCREEN BELOW TO THIS FORMAT & REMOVE FROM MAIN WINDOW 1
+    
+    
---
+++ pseudo_main.py
@@ -0,0 +1,39 @@
+# Main Pseudo code
+
+# File Creation | Initialization Pg 0 | Dialog 1
+    # If New Simulation
+        # Make copy of template
+        # Input new name file
+        # Rename template copy
+    # Load Old Simulation
+        # Select old file
+        # If Copy
+            # Set new name
+        # If Edit
+    # Will handle the file name of whatever excel is being used
+
+# Define Architecture | Main Window 1 | Pg 1
+    # User uses radio buttons to select system components (& coolant?)
+    # Could use text box next to radio to enter order in circuit? (1, 2, 3...)
+    # Display Image graphically displaying the circuit
+        # Node library used at Burns may make this easy
+
+# Initialize Parameters | Main Window 1 | Pg 2-#
+    # Initialize System Parameters (Water, Air, & FoS)
+    # Input each Component Parameters (Pg 3-#)
+    # Try to have tab visibility based on Pg 1 selection
+    # May have to choose ind\dep var here
+
+# Finalize Set-up | Main Window 1 | Pg #+1 (lol)
+    # Try to allow them to review their sim
+    # Would be great to have a picture of the circuit w/ parameters labeled & marked ind\dep
+
+# Calculations | Dialog
+    # Just some cool stuff to make it seem like the app is doing something
+    # If using some scipy function, could display the solver output text 
+
+# Results (Setpoint) | Main Window 2 | Pg 1
+    # Dashboard Showing Inputs
+    # Dashboard Shwoing Resulting values
+    # Maybe graph showing soling point
+    
