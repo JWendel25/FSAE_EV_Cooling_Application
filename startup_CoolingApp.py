056e26faf383f1e3c66867514ea7d6f6dfe5d200
Joshua Wendel <joshua.w.wendel@okstate.edu>
2025-03-27 15:10:16 -0500

    Startup Finalization
    Finalized the UI & Script for the Startup of the app. Will need to be developed to attach to the launch of the next stage of the app.
    
    Application now allows users to choose the file where they will preform the simulation.

--- startup_CoolingApp.py
+++ startup_CoolingApp.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import shutil
+import pathlib
 
 # Import PyQt5 classes for GUI
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QCursor
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog
 
 # Import UI
 from UI_Files.qt_Startup import Ui_Dialog
@@ -19,16 +20,25 @@ class startup_window(QtWidgets.QMainWindow, QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         
-        # Initialize other parameters
+        # Initialize Parameters
         self.ui.stackedWidget.setCurrentIndex(0)
+        self.simFilePath = None
+        self.copyFileName = None
+        self.editFileName = None
         self.previousIndex = None
+        self.errorMessage = None
         
+        # Start UI
         self.assign_widgets()
         self.show()
     
+    
     def fill(self):
-        print("Not yet tied to a function")
+        print('Not yet tied to a function')
         
+    
+    # NAVIGATION DEFINITIONS
+    
     def nav_new(self):
         self.ui.stackedWidget.setCurrentIndex(1)
 
@@ -38,31 +48,90 @@ class startup_window(QtWidgets.QMainWindow, QDialog):
     def nav_edit(self):
         self.ui.stackedWidget.setCurrentIndex(3)
     
-    def launch_new(self):
-        newFileName = self.ui.textEdit_NewSim.toPlainText() + ".xlsm"
-        newFilePath = os.path.join('Simulations', newFileName)
-        shutil.copy(r'Simulations/Template.xlsm', newFilePath)
-        print('Sucess') # Need to add error handle if no text is entered
-        # Add opening next stage of app
-    
-    def select_file_copy(self):
-        pass
-    
-    def launch_copy(self):
-        pass
-    
-    def select_file_edit(self):
-        pass
-    
-    def launch_edit(self):
-        pass
-    
     def back(self):
         self.ui.stackedWidget.setCurrentIndex(0)
+        self.copyFileName = None
+        self.editFileName = None
+    
+    
+    # NEW SIMULATION DEFINITIONS
+    
+    def launch_new(self):
+        newNameEntered = self.ui.textEdit_New.toPlainText()
+        if newNameEntered != '' :
+            newFileName = newNameEntered + '.xlsm'
+            newFilePath = os.path.join('Simulations', newFileName)
+            shutil.copy(r'Simulations/Template.xlsm', newFilePath)
+            print('Sucess')
+            self.simFilePath = newFilePath
+            # Add opening next stage of app
+        else:
+            self.errorMessage = 'Error: Failed to enter file name. Please hit the \"OK\" button and enter a name into the given text box before launching the simulation.'
+            self.error_send()
+    
+    
+    # NEW SIMULATION FROM COPY DEFINITIONS
+    
+    def select_file_copy(self):
+        copyFilePath = QFileDialog.getOpenFileName(None, 'Select File', os.path.join(pathlib.Path().resolve(), 'Simulations'))[0]                        # Stores the opened file name as a self variable
+        QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))                      # Dispalys a loading cursor
+        app.processEvents()                                                         # Ensures the app will remain responsive after opening the file
+        QApplication.restoreOverrideCursor()                                        # Turns of the loading cursor once the app has finished processing
+        self.copyFileName = os.path.basename(copyFilePath[:-5])
+        self.ui.textEdit_Copy.setText(self.copyFileName)                          # Displays the file name in the dedicated text bar
+    
+    def launch_copy(self):
+        newNameEntered = self.ui.textEdit_Copy.toPlainText()
+        if self.copyFileName == None :
+            self.errorMessage = 'Error: No file was selected. Please hit the \"OK\" button and select a file before launching the simulation.'
+            self.error_send()
+        elif newNameEntered != '' and newNameEntered != self.copyFileName :
+            newFileName = newNameEntered + '.xlsm'
+            newFilePath = os.path.join('Simulations', newFileName)
+            copyFilePath = os.path.join('Simulations', self.copyFileName + '.xlsm')
+            shutil.copy(copyFilePath, newFilePath)
+            print('Sucess')
+            self.simFilePath = newFilePath
+            # Add opening next stage of app
+        else:
+            if newNameEntered == self.copyFileName :
+                self.errorMessage = 'Error: Failed to change file name. Please hit the \"OK\" button and enter a name into the given text box before launching the simulation.'
+            else:
+                self.errorMessage = 'Error: Failed to enter file name. Please hit the \"OK\" button and enter a name into the given text box before launching the simulation.'
+            self.error_send()
+            
+            
+    # EDIT SIMULATION DEFINITIONS
+    
+    def select_file_edit(self):
+        editFilePath = QFileDialog.getOpenFileName(None, 'Select File', os.path.join(pathlib.Path().resolve(), 'Simulations'))[0]                        # Stores the opened file name as a self variable
+        QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))                      # Dispalys a loading cursor
+        app.processEvents()                                                         # Ensures the app will remain responsive after opening the file
+        QApplication.restoreOverrideCursor()                                        # Turns of the loading cursor once the app has finished processing
+        self.editFileName = os.path.basename(editFilePath[:-5])
+        self.ui.textEdit_Edit.setText(self.editFileName)                          # Displays the file name in the dedicated text bar
+    
+    def launch_edit(self):
+        if self.editFileName == None :
+            self.errorMessage = 'Error: No file was selected. Please hit the \"OK\" button and select a file before launching the simulation.'
+            self.error_send()
+        else:
+            print('Sucess')
+            self.simFilePath = self.editFileName
+            # Add opening next stage of app
+        
+        
+    # ERROR HANDLING DEFINITIONS
+    
+    def error_send(self):
+        self.previousIndex = self.ui.stackedWidget.currentIndex()
+        self.ui.textDisplayError.setText(self.errorMessage)
+        self.ui.stackedWidget.setCurrentIndex(4)
     
     def error_return(self):
         self.ui.stackedWidget.setCurrentIndex(self.previousIndex)
-
+    
+    
     def assign_widgets(self):
         # Landing Page Buttons
         self.ui.NavNew.clicked.connect(self.nav_new)
@@ -74,13 +143,13 @@ class startup_window(QtWidgets.QMainWindow, QDialog):
         self.ui.BackNew.clicked.connect(self.back)
         
         # New from Copy Buttons
-        self.ui.SelectFileCopy.clicked.connect(self.fill)
-        self.ui.LaunchCopy.clicked.connect(self.fill)
+        self.ui.SelectFileCopy.clicked.connect(self.select_file_copy)
+        self.ui.LaunchCopy.clicked.connect(self.launch_copy)
         self.ui.BackCopy.clicked.connect(self.back)
         
         # Edit Previous Buttons
-        self.ui.SelectFileEdit.clicked.connect(self.fill)
-        self.ui.LaunchEdit.clicked.connect(self.fill)
+        self.ui.SelectFileEdit.clicked.connect(self.select_file_edit)
+        self.ui.LaunchEdit.clicked.connect(self.launch_edit)
         self.ui.BackEdit.clicked.connect(self.back)
         
         # Error Buttons
